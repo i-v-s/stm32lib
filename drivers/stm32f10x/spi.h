a3e974ddf7e833a4aed14169e63b22ca0cfc74c9
Igor Smirnov <smirnoviv@rambler.ru>
2018-10-20 08:17:59 +0500

    Add pin remap and buses for SPI

--- drivers/stm32f10x/spi.h
+++ drivers/stm32f10x/spi.h
@@ -58,6 +58,38 @@ public:
         p().CR1 = p().CR1 & ~SPI_CR1_BR_Msk | boudrate2cr1<rate>();
     }
     inline void enable() { p().CR1 |= SPI_CR1_SPE; }
+    inline static constexpr uint32_t ahb() { return 0; }
+    inline static constexpr uint32_t apb1() { 
+        switch(spi) {
+        default: return 0;
+        case uintptr_t(SPI2): return RCC_APB1ENR_SPI2EN;
+#ifdef SPI3
+        case uintptr_t(SPI3): return RCC_APB1ENR_SPI3EN;
+#endif
+        }
+    }
+    inline static constexpr uint32_t apb2() { return (spi == uintptr_t(SPI1)) ? RCC_APB2ENR_SPI1EN : 0; }
+    static_assert(apb1() ^ apb2(), "No bus specified for spi!");
 };
+
+template<typename Device> struct MISO {};
+template<typename Device> struct MOSI {};
+template<typename Device> struct SCK  {};
+template<typename Device> struct NSS  {};
+
+
+#define SPI_REMAP(spi, func, port, pin, mask, val) template<typename Clock, const Clock *clock> \
+struct Remap<func<Spi<uintptr_t(spi), Clock, clock>>, Pin<uintptr_t(port), pin>> \
+    { enum { maprMsk = mask, maprVal = val ? mask : 0 }; }
+
+SPI_REMAP(SPI1, NSS,  GPIOA, 4,  AFIO_MAPR_SPI1_REMAP, 0);
+SPI_REMAP(SPI1, SCK,  GPIOA, 5,  AFIO_MAPR_SPI1_REMAP, 0);
+SPI_REMAP(SPI1, MISO, GPIOA, 6,  AFIO_MAPR_SPI1_REMAP, 0);
+SPI_REMAP(SPI1, MOSI, GPIOA, 7,  AFIO_MAPR_SPI1_REMAP, 0);
+
+SPI_REMAP(SPI1, NSS,  GPIOA, 15, AFIO_MAPR_SPI1_REMAP, 1);
+SPI_REMAP(SPI1, SCK,  GPIOB, 3,  AFIO_MAPR_SPI1_REMAP, 1);
+SPI_REMAP(SPI1, MISO, GPIOB, 4,  AFIO_MAPR_SPI1_REMAP, 1);
+SPI_REMAP(SPI1, MOSI, GPIOB, 5,  AFIO_MAPR_SPI1_REMAP, 1);
 
 #endif
